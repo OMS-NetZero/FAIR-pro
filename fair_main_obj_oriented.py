99e745ca4e907498843b6a1fcbb438a039027f2f
richardcode <richardmillar728@gmail.com>
2017-03-30 13:31:50 +0100

    Updated version of OO-FAIR with timestep method

--- fair_main_obj_oriented.py
+++ fair_main_obj_oriented.py
@@ -6,121 +6,139 @@ import numpy as np
 from scipy.optimize import root
 
 # # # ------------ CODE ------------ # # #
-class FAIR:
-	"""
-	Documentation string:
-	FAIR class for running the forward emissions driven FAIR model.
+class FAIR(object):
+    """Class for computing with the FAIR simple climate-carbon-cycle model.
+        """
+    def __init__(self, emissions=np.array([0.,1.,2.,3.]), other_rf=0.0,TCR=1.75,ECS=2.5,d=np.array([4.1,239.0]),a=np.array([0.2173,0.2240,0.2824,0.2763]),tau=np.array([1000000,394.4,36.54,4.304]),r0=32.40,rc=0.019,rt=4.165,F_2x=3.74,C_0=278.0,ppm_gtc=2.123,iirf_max=97.0):
+        """FAIR init
+            Args:
+            :param emissions: Array of annual emissions in GtC
+            :type emissions: numpy 1D array
+            :param other_rf: Time series of other radiative forcing or float for constant value
+            :type time_step: float/numpy 1D array
+            :param tcrecs: Numpy array of the specified TCR and ECS values
+            """
 
-	# # # ------------ KEY VARIABLES ------------ # # #
-	variable: (type) Explanation/description
+        #Assign the initialized parameters to the class data members
+        if emissions is not None and type(emissions) in [list, np.ndarray]:
+            self.emissions = np.array(emissions)
+        
+        self.other_rf = other_rf
 
-	# # # ------------ SET KEY VARIABLES ------------ # # #
+        self.TCR = TCR
+        self.ECS = ECS
+        self.d = d
+        self.a = a
+        self.tau = tau
+        self.r0 = r0
+        self.rc = rc
+        self.rt = rt
+        self.F_2x = F_2x
+        self.C_0 = C_0
+        self.ppm_gtc = ppm_gtc
+        self.iirf_max = iirf_max
+        
+        #Get the length of the emissions array
+        self.n = len(emissions)
 
-	# # # ------------ KEY METHODS ------------ # # #
-	# __init__: initialise an instance of the FAIR class
+        #Calculate the q1 and q2 model coefficients from the TCR, ECS and thermal response timescales.
+        self.k = 1.0 - (self.d/70.0)*(1.0 - np.exp(-70.0/self.d))
+        self.q = np.transpose((1.0 / self.F_2x) * (1.0/(self.k[0]-self.k[1])) * np.array([self.TCR-self.ECS*self.k[1],self.ECS*self.k[0]-self.TCR]))
+    
 
-	# run: run the model
+    def iirf_interp_funct(self,alp_b,iirf_targ):
 
-	# # # ------------ WORKFLOW SUMMARY ------------ # # #  
+        iirf_arr = alp_b*(np.sum(self.a*self.tau*(1.0 - np.exp(-100.0/(self.tau*alp_b)))))
+        return iirf_arr   -  iirf_targ
+    
+    def get_tau_sf(self,iirf_targ):
+        
+        if self.x == 1:
+          return (root(self.iirf_interp_funct,0.16,args=(iirf_targ)))['x']
+        else:
+          return (root(self.iirf_interp_funct,self.sf,args=(iirf_targ)))['x']
 
-	"""
+    def time_step(self):
 
-	def __init__(self,emissions):
-		self.emissions = np.array(emissions,dtype=float)
-		# set default parameters
-		self.other_rf = 0.0
-		self.TCR = 1.75
-		self.ECS = 2.5
-		self.d=np.array([4.1,239.0])
-		self.a=np.array([0.2173,0.2240,0.2824,0.2763])
-		self.tau=np.array([1000000,394.4,36.54,4.304])
-		self.r0=32.40
-		self.rC=0.019
-		self.rT=4.165
-		self.F_2x=3.74
-		self.C_0=278.0
-		self.ppm_gtc=2.123
-		self.iirf_max=97.0
+        self.IIRF100_x = self.rc * self.C_acc_x + self.rt * self.T_x + self.r0
+        if self.IIRF100_x >= self.iirf_max:
+           self.IIRF100_x = self.iirf_max
+        self.sf = self.get_tau_sf(self.IIRF100_x)
 
-	def iirf_interp_funct(self,alp_b,a,tau,iirf,x):
-		# it seems overly convoluted to me to have iirf and x as separate arguments to this function surely it would be much more flexible and sensible to just have the last argument be the target iirf (so this iirf_interp_funct could be used to just determine the scaling factor for a given iirf) rather than having to make sure you have an array and index the right element
+        tau_new = self.tau * self.sf
 
-		# ref eq. (7) of Millar et al ACP (2017)
-	    
-	    iirf_arr = alp_b*(np.sum(a*tau*(1.0 - np.exp(-100.0/(tau*alp_b)))))
-	    return iirf_arr   -  iirf[x]
+        self.R_i_x = self.R_i_x*np.exp(-1.0/tau_new) + self.a*(self.emissions_x) / self.ppm_gtc
+        #Summ the boxes to get the total concentration anomaly
+        self.C_x_old = self.C_x
+        self.C_x = np.sum(self.R_i_x,axis=-1)
+        #Calculate the additional carbon uptake
+        self.C_acc_x =  self.C_acc_x + 0.5*(self.emissions_x +self.emissions_x_old) - (self.C_x - self.C_x_old)*self.ppm_gtc
 
-	def run(self):
-		#Calculate the q1 and q2 model coefficients from the TCR, ECS and thermal response timescales.
-		# ref eq. (4) and (5) of Millar et al ACP (2017)
-		k = 1.0 - (self.d/70.0)*(1.0 - np.exp(-70.0/self.d))
-		q = np.transpose( (1.0 / self.F_2x) * (1.0/(k[0]-k[1])) * np.array([self.TCR-self.ECS*k[1],self.ECS*k[0]-self.TCR]))
+        #Calculate the total radiative forcing
+        if type(self.other_rf) == float:
+           self.RF_x = (self.F_2x/np.log(2.)) * np.log((self.C_x + self.C_0) /self.C_0) + self.other_rf
+        else:
+           self.RF_x = (self.F_2x/np.log(2.)) * np.log((self.C_x + self.C_0) /self.C_0) + self.other_rf_x
 
-		#Set up the output timeseries variables
-		# self.emissions must be a numpy array for this to work
-		carbon_boxes_shape = tuple(list(self.emissions.shape) + [4])
-		thermal_boxes_shape = tuple(list(self.emissions.shape) + [2])
+        #Update the thermal response boxes
+        self.T_j_x = self.T_j_x*np.exp(-1.0/self.d) + (self.q/self.d)*self.RF_x
+        #Sum the thermal response boxes to get the total temperature anomlay
+        self.T_x=np.sum(self.T_j_x,axis=-1)
 
-		RF = np.zeros_like(self.emissions)
-		C_acc = np.zeros_like(self.emissions)
-		iirf = np.zeros_like(self.emissions)
-		R_i = np.zeros(carbon_boxes_shape)
-		T_j = np.zeros(thermal_boxes_shape)
 
-		C = np.zeros_like(self.emissions)
-		T = np.zeros_like(self.emissions)
 
-		#Initialise the carbon pools to be correct for first timestep in numerical method
-		R_i[0,:] = self.a * self.emissions[0,np.newaxis] / self.ppm_gtc
-		C[0] = np.sum(R_i[0,:],axis=-1)
-		if type(self.other_rf) == float:
-			RF[0] = (self.F_2x/np.log(2.)) * np.log((C[0] + self.C_0) /self.C_0) + self.other_rf
-		else:
-			RF[0] = (self.F_2x/np.log(2.)) * np.log((C[0] + self.C_0) /self.C_0) + self.other_rf[x]
-		#Update the thermal response boxes
-		T_j[0,:] = (q/self.d)*(RF[0,np.newaxis])
-		#Sum the thermal response boxes to get the total temperature anomlay
-		T[0]=np.sum(T_j[0,:],axis=-1)
-
-		for x in range(1,self.emissions.shape[-1]):
-		  
-			#Calculate the parametrised iIRF and check if it is over the maximum allowed value
-			iirf[x] = self.rC * C_acc[x-1]  + self.rT*T[x-1]  + self.r0
-			if iirf[x] >= self.iirf_max:
-			  iirf[x] = self.iirf_max
-		  
-			#Linearly interpolate a solution for alpha
-			if x == 1:
-			  time_scale_sf = (root(self.iirf_interp_funct,0.16,args=(self.a,self.tau,iirf,x)))['x']
-			else:
-			  time_scale_sf = (root(self.iirf_interp_funct,time_scale_sf,args=(self.a,self.tau,iirf,x)))['x']
-
-			#Multiply default timescales by scale factor
-			tau_new = self.tau * time_scale_sf
-
-			#Compute the updated concentrations box anomalies from the decay of the pervious year and the additional emisisons
-			R_i[x,:] = R_i[x-1,:]*np.exp(-1.0/tau_new) + self.a*(self.emissions[x,np.newaxis]) / self.ppm_gtc
-			#Summ the boxes to get the total concentration anomaly
-			C[x] = np.sum(R_i[...,x,:],axis=-1)
-			#Calculate the additional carbon uptake
-			# Do we want to keep this 0.5 factor here? We seem to have rid ourselves of it everywhere else...
-			# The way this timing stuff is handled in MAGICC is quite nice and may 
-			# provide us with guidance on how to do it ourselves if we want
-			C_acc[x] =  C_acc[x-1] + 0.5*(self.emissions[x] + self.emissions[x-1]) - (C[x] - C[x-1])*self.ppm_gtc
-
-			#Calculate the total radiative forcing
-			if type(self.other_rf) == float:
-				RF[x] = (self.F_2x/np.log(2.)) * np.log((C[x] + self.C_0) /self.C_0) + self.other_rf
-			else:
-				RF[x] = (self.F_2x/np.log(2.)) * np.log((C[x] + self.C_0) /self.C_0) + self.other_rf[x]
-
-			#Update the thermal response boxes
-			T_j[x,:] = T_j[x-1,:]*np.exp(-1.0/self.d) + (q/self.d)*(RF[x,np.newaxis])
-			#Sum the thermal response boxes to get the total temperature anomaly
-			T[x]=np.sum(T_j[x,:],axis=-1)
-
-		self.C = C + self.C_0
-		self.T = T
+    def run(self):
+        
+      #Set up the output arrays
+      self.R_i = np.zeros((self.n,4))
+      self.T_j = np.zeros((self.n,2))
+      self.C = np.zeros((self.n))
+      self.T = np.zeros((self.n))
+      self.C_acc =np.zeros((self.n))
+      self.IRF100 = np.zeros((self.n))
+      self.RF = np.zeros((self.n))
+          
+      #Initialise the carbon pools to be correct for first timestep in numerical method
+      self.R_i_x = self.a * self.emissions[0] / self.ppm_gtc
+      self.C_x = np.sum(self.R_i_x,axis=-1)
+      if type(self.other_rf) == float:
+          self.RF_x = (self.F_2x/np.log(2.)) * np.log((self.C_x + self.C_0) /self.C_0) + self.other_rf
+      else:
+          self.RF_x = (self.F_2x/np.log(2.)) * np.log((self.C_x + self.C_0) /self.C_0) + self.other_rf[0]
+      #Update the thermal response boxes
+      self.T_j_x = self.T_j[0]*np.exp(-1.0/self.d) + (self.q/self.d)*self.RF_x
+      #Sum the thermal response boxes to get the total temperature anomlay
+      self.T_x=np.sum(self.T_j_x,axis=-1)
+      self.C_acc_x = 0.0
+      self.emissions_x_old = self.emissions[0]
+          
+      self.R_i[0] = self.R_i_x
+      self.T_j[0] = self.T_j_x
+      self.C[0] = self.C_x
+      self.T[0] = self.C_x
+      self.C_acc[0] = self.C_acc_x
+      self.IRF100[0] = self.rc * self.C_acc_x + self.rt * self.T_x + self.r0
+      self.RF[0] = self.RF_x
+          
+      for x in range(1,self.n):
+          
+          self.x = x
+          self.emissions_x = self.emissions[x]
+          self.emissions_x_old = self.emissions[x-1]
+          #Run a timestep
+          self.time_step()
+          #Save the timestep output
+          self.R_i[x] = self.R_i_x
+          self.T_j[x] = self.T_j_x
+          self.C[x] = self.C_x
+          self.T[x] = self.C_x
+          self.C_acc[x] = self.C_acc_x
+          self.IRF100[x] = self.IIRF100_x
+          self.RF[x] = self.RF_x
+      
+    
+      #Add on the pre-industrial concentrations to the carbon concentration output array
+      self.C = self.C + self.C_0
 
 	def printPara(self):
 		print 'TCR: ', self.TCR
