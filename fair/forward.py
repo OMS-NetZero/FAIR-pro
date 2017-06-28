22f35e989ddf92269b9b68153a9b6ecd75a5ebb6
njleach <njleach.1@gmail.com>
2017-06-28 20:15:21 +0100

    Add CH4 and N2O into the model
    Have added Methane and N2O into the model in as basic a way as possible with fixed lifetimes. I've defined a few more functions in order to keep the code from being too repetitive or long, but on sections that I've majorly altered, I've just commented out the previous code so reverting back if desired is easier.

--- fair/forward.py
+++ fair/forward.py
@@ -62,24 +62,61 @@ def iirf100_interp_funct(alpha,a,tau,targ_iirf100):
     iirf100_arr = alpha*(np.sum(a*tau*(1.0 - np.exp(-100.0/(tau*alpha)))))
     return iirf100_arr   -  targ_iirf100
 
+# define a function that: for 4 inputs and a length L:
+# leaves arrays as they are
+# converts scalar types into a array of length L, all elements the same scalar value (constant emissions or concentrations)
+# converts False types (ie. no argument given) into an array of length L of zeros
+# checks the all the final arrays have the same length and raises an error if not
+def emissions_concentrations_sort(A,B,C,D,length):
+    Arr = [A,B,C,D]
+    for x in range(len(Arr)):
+        if type(Arr[x]) in [int,float]:
+            Arr[x] = np.full(length,Arr[x])
+        elif type(Arr[x]) == bool:
+            Arr[x] = np.zeros(length)
+    if any(len(x) != length for x in Arr):
+	    raise ValueError("One or more of the emissions/concentrations given or other_rf timeseries doesn't have the same length")
+    else:
+	    return Arr
+
+# Define three functions that give the Radiative forcing due to CH4 and N2O as per equations given in IPCC AR5 8.SM
+def f(M, N):
+	return 0.47 * np.log(1 + 2.01 * 10**(-5) * M * N * 0.75 + 5.31 * 10**(-15) * M * (M * N)**(1.52)) # see IPCC AR5 Table 8.SM.1
+
+def RF_M(M, N, M_0, N_0, alp_m=0.036):
+	return alp_m * (np.sqrt(M+M_0) - np.sqrt(M_0)) - (f(M+M_0, N_0) - f(M_0, N_0))
+
+def RF_N(M, N, M_0, N_0, alp_n=0.12):
+	return alp_n * (np.sqrt(N+N_0) - np.sqrt(N_0)) - (f(M_0, N+N_0) - f(M_0, N_0))
+
 # Define the FAIR simple climate model function
 def fair_scm(tstep=1.0,
              emissions=False,
+			 M_emissions=False,
+			 N_emissions=False,
              other_rf=0.0,
              co2_concs=False,
+			 M_concs=False,
+			 N_concs=False,
              q=np.array([0.33,0.41]),
              tcrecs=np.array([1.6,2.75]),
              d=np.array([239.0,4.1]),
              a=np.array([0.2173,0.2240,0.2824,0.2763]),
              tau=np.array([1000000,394.4,36.54,4.304]),
+			 tau_M=12.4,
+			 tau_N=121,
              r0=32.40,
              rC=0.019,
              rT=4.165,
              F_2x=3.74,
              C_0=278.0,
+			 M_0=722,
+			 N_0=270,
              ppm_gtc=2.123,
+			 ppb_TgM=2.838,
+			 ppb_TgN=7.787,
              iirf100_max=97.0,
-             in_state=[[0.0,0.0,0.0,0.0],[0.0,0.0],0.0],
+             in_state=[[0.0,0.0,0.0,0.0],[0.0,0.0],0.0,0.0,0.0],
              restart_out=False):
     """
     Run fair forward calculation
@@ -97,6 +134,16 @@ def fair_scm(tstep=1.0,
       CO_2 emissions timeseries (GtC/yr). If a scalar then emissions are 
       assumed to be constant throughout the run. If false then emissions 
       aren't used.
+	
+    M_emissions:^ (np.array/list/float/int/bool)
+      CH4 emissions timeseries (Tg/yr). If a scalar then emissions are 
+      assumed to be constant throughout the run. If false then CH4 emissions 
+      aren't used.
+	  
+    N_emissions:^ (np.array/list/float/int/bool)
+      N2O emissions timeseries (Tg/yr). If a scalar then emissions are 
+      assumed to be constant throughout the run. If false then N2O emissions 
+      aren't used.
 
     other_rf:^ (np.array/list/float/int)
       Non-CO_2 radiative forcing timeseries (W/m^2). If a scalar then other_rf 
@@ -105,6 +152,14 @@ def fair_scm(tstep=1.0,
     co2_concs:^ (np.array/list/bool)
       Atmospheric CO_2 concentrations timeseries (ppmv). If emissions are 
       supplied then co2_concs is not used.
+	  
+    M_concs:^ (np.array/list/bool)
+      Atmospheric CH4 concentrations timeseries (ppbv). If M_emissions are 
+      supplied then M_concs is not used.
+	  
+    N_concs:^ (np.array/list/bool)
+      Atmospheric N2O concentrations timeseries (ppbv). If N_emissions are 
+      supplied then N_concs is not used.
 
     q:^ (np.array)
       response of each thermal box to radiative forcing (K/(W/m^2)). 
@@ -124,6 +179,12 @@ def fair_scm(tstep=1.0,
 
     tau:^ (np.array)
       unscaled response time of each carbon pool (yrs)
+	  
+    tau_M:^ (float/int)
+      lifetime of atmospheric CH4 (yrs) (value used for IPCC AR5 metrics)
+	  
+    tau_N:^ (float/int)
+      lifetime of atmospheric N2O (yrs) (value used for IPCC AR5 metrics)
 
     r0:^ (float)
       pre-industrial 100-year integrated impulse response (iIRF100) (yrs)
@@ -140,9 +201,21 @@ def fair_scm(tstep=1.0,
 
     C_0:^ (float)
       pre-industrial atmospheric CO_2 concentrations (ppmv)
+	  
+    M_0:^ (float)
+      pre-industrial atmospheric CH4 concentrations (ppbv)
+	  
+    N_0:^ (float)
+      pre-industrial atmospheric N2O concentrations (ppbv)
 
     ppm_gtc:^ (float)
       ppmv to GtC conversion factor (GtC/ppmv)
+	  
+    ppb_TgM:^ (float)
+      ppbv to Tg of CH4 conversion factor (TgCH4/ppbv)
+	  
+    ppb_TgN:^ (float)
+      ppbv to Tg of N2O conversion factor (TgN2O/ppbv)
 
     iirf100_max:^ (float)
       maximum allowed value of iIRF100 (keeps the model stable) (yrs)
@@ -155,6 +228,10 @@ def fair_scm(tstep=1.0,
           temp of each temperature response box (K)
         [2]: (float)
           cumulative carbon uptake (GtC)
+        [3]: (float)
+          CH4 concentration (ppbv)
+        [4]: (float)
+          N2O concentration (ppbv)
 
     restart_out:^ (bool)
       whether to return the final state of the climate system or not
@@ -212,30 +289,44 @@ def fair_scm(tstep=1.0,
     # the integ_len variable is used to store the length of our timeseries
     # by default FAIR is not concentration driven
     conc_driven=False
-    # here we check if FAIR is emissions driven
+    
+	# here we check if we want to include CH4 or N2O ie. if they are given as arguments in fair_scm
+    if all(type(x) == bool for x in [M_emissions,N_emissions,M_concs,N_concs]):
+        include_M_N = [False,False]
+    elif any(type(x) != bool for x in [M_emissions,M_concs]):
+	    include_M_N = [True,False]
+    elif any(type(x) != bool for x in [N_emissions,N_concs]):
+	    include_M_N = [False,True]
+    else:
+	    include_M_N = [True,True]
+	
+	# here we check if FAIR is emissions driven, for now assuming if CO_2 is emissions driven, the other GHGs are as well
     if type(emissions) in [np.ndarray,list]:
         integ_len = len(emissions)
-        if (type(other_rf) in [np.ndarray,list]) and (len(other_rf)!=integ_len):
-            raise ValueError("The emissions and other_rf timeseries don't have the same length")
-        elif type(other_rf) in [int,float]:
-            other_rf = np.full(integ_len,other_rf)
+        [emissions,M_emissions,N_emissions,other_rf] = emissions_concentrations_sort(emissions,M_emissions,N_emissions,other_rf,integ_len)
+        # if (type(other_rf) in [np.ndarray,list]) and (len(other_rf)!=integ_len):
+            # raise ValueError("The emissions and other_rf timeseries don't have the same length")
+        # elif type(other_rf) in [int,float]:
+            # other_rf = np.full(integ_len,other_rf)
   
     # here we check if FAIR is concentration driven
     elif type(co2_concs) in [np.ndarray,list]:
         integ_len = len(co2_concs)
         conc_driven = True
-        if (type(other_rf) in [np.ndarray,list]) and (len(other_rf)!=integ_len):
-            raise ValueError("The concentrations and other_rf timeseries don't have the same length")
-        elif type(other_rf) in [int,float]:
-            other_rf = np.full(integ_len,other_rf)
+        [co2_concs,M_concs,N_concs,other_rf,integ_len] = emissions_concentrations_sort(co2_concs,M_concs,N_concs,other_rf,integ_len)
+        # if (type(other_rf) in [np.ndarray,list]) and (len(other_rf)!=integ_len):
+            # raise ValueError("The concentrations and other_rf timeseries don't have the same length")
+        # elif type(other_rf) in [int,float]:
+            # other_rf = np.full(integ_len,other_rf)
 
     # finally we check if only a non-CO2 radiative forcing timeseries has been supplied
     elif type(other_rf) in [np.ndarray,list]:
         integ_len = len(other_rf)
-        if type(emissions) in [int,float]:
-            emissions = np.full(integ_len,emissions)
-        else:
-            emissions = np.zeros(integ_len)
+        [emissions,M_emissions,N_emissions,other_rf] = emissions_concentrations_sort(emissions,M_emissions,N_emissions,other_rf,integ_len)
+        # if type(emissions) in [int,float]:
+            # emissions = np.full(integ_len,emissions)
+        # else:
+            # emissions = np.zeros(integ_len)
 
     else:
         raise ValueError("Neither emissions, co2_concs or other_rf is defined as a timeseries")
@@ -247,7 +338,9 @@ def fair_scm(tstep=1.0,
     carbon_boxes_shape = (integ_len,4)
     R_i = np.zeros(carbon_boxes_shape)
     C = np.zeros(integ_len)
-
+    M = np.zeros(integ_len)
+    N = np.zeros(integ_len)
+    
     thermal_boxes_shape = (integ_len,2)
     T_j = np.zeros(thermal_boxes_shape)
     T = np.zeros(integ_len)
@@ -257,9 +350,13 @@ def fair_scm(tstep=1.0,
     C_pre = np.sum(R_i_pre) + C_0
     T_j_pre = in_state[1]
     C_acc_pre = in_state[2]
+    M_pre = in_state[3]
+    N_pre = in_state[4]
 
     if conc_driven:
         C[0] = co2_concs[0]
+        M[0] = M_concs[0]
+        N[0] = N_concs[0]
   
     else:
         # Calculate the parametrised iIRF and check if it is over the maximum 
@@ -280,13 +377,18 @@ def fair_scm(tstep=1.0,
                   + (emissions[0,np.newaxis])*a*tau_new*(1-np.exp(-tstep/tau_new)) / ppm_gtc
 
         C[0] = np.sum(R_i[0]) + C_0
+        
+        # Compute the concentrations of the other GHGs from the decay of the previous year and yearly emissions
+        M[0] = M_pre*np.exp(-tstep/tau_M) + M_emissions[0]*tau_M*(1-np.exp(-tstep/tau_M)) / ppb_TgM
+        
+        N[0] = N_pre*np.exp(-tstep/tau_N) + N_emissions[0]*tau_N*(1-np.exp(-tstep/tau_N)) / ppb_TgN
 
         # Calculate the additional carbon uptake
         C_acc[0] =  C_acc_pre + emissions[0] - (C[0]-(np.sum(R_i_pre) + C_0)) * ppm_gtc
 
     # Calculate the radiative forcing using the previous timestep's CO2 concentration
 
-    RF[0] = (F_2x/np.log(2.)) * np.log(C_pre/C_0) + other_rf[0]
+    RF[0] = (F_2x/np.log(2.)) * np.log(C_pre/C_0) + other_rf[0] + RF_M(M_pre,N_pre,M_0,N_0) + RF_N(M_pre,N_pre,M_0,N_0)
 
     # Update the thermal response boxes
     T_j[0] = RF[0,np.newaxis]*q*(1-np.exp((-tstep)/d)) + T_j_pre*np.exp(-tstep/d)
@@ -298,6 +400,8 @@ def fair_scm(tstep=1.0,
     for x in range(1,integ_len):
         if conc_driven:
           C[x] = co2_concs[x]
+          M[x] = M_concs[x]
+          N[x] = N_concs[x]
         
         else:
           # Calculate the parametrised iIRF and check if it is over the maximum 
@@ -318,12 +422,17 @@ def fair_scm(tstep=1.0,
 
           # Sum the boxes to get the total concentration anomaly
           C[x] = np.sum(R_i[x]) + C_0
+          
+          # Compute the concentrations for the other GHGs from the decay of previous year and yearly emissions
+          M[x] = M[x-1]*np.exp(-tstep/tau_M) + M_emissions[x]*tau_M*(1-np.exp(-tstep/tau_M)) / ppb_TgM
+          
+          N[x] = N[x-1]*np.exp(-tstep/tau_N) + N_emissions[x]*tau_N*(1-np.exp(-tstep/tau_N)) / ppb_TgN
 
           # Calculate the additional carbon uptake
           C_acc[x] =  C_acc[x-1] + emissions[x] * tstep - (C[x]-C[x-1]) * ppm_gtc
 
         # Calculate the radiative forcing using the previous timestep's CO2 concentration
-        RF[x] = (F_2x/np.log(2.)) * np.log((C[x-1]) /C_0) + other_rf[x]
+        RF[x] = (F_2x/np.log(2.)) * np.log((C[x-1]) /C_0) + other_rf[x] + RF_M(M[x-1],N[x-1],M_0,N_0) + RF_N(M[x-1],N[x-1],M_0,N_0)
 
         # Update the thermal response boxes
         T_j[x] = T_j[x-1]*np.exp(-tstep/d) + RF[x,np.newaxis]*q*(1-np.exp(-tstep/d))
