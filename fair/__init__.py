7a6d5f23604f3478b173fbb53202e1994cb2afc2
ZebedeeN <zebedee.nicholls@climate-energy-college.org>
2017-07-17 20:43:32 +1000

    Re-arrange files

---
+++ fair/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: zebedeen
+# @Date:   2017-07-17 20:42:49
+# @Last Modified by:   ZebedeeN
+# @Last Modified time: 2017-07-17 20:43:04
+# # # ------------ IMPORT REQUIRED MODULES ------------ # # #
+from _version import __version__
