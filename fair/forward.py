bf3354cfd730ca97fc3be2623d9d319198a54cda
OllieBraddy <braddyoliver@gmail.com>
2017-07-27 14:51:32 +0100

    multi params and rf profiles
    Allows the input of 2D parameter array, also 2D other_rf array. All works fine for normal input etc. Also added the ability to print the percentage completed and predicted time remaining for large computations.

--- fair/forward.py
+++ fair/forward.py
@@ -59,26 +59,15 @@ def iirf100_interp_funct(alpha,a,tau,targ_iirf100):
     # # ------------ LOCAL APPLICATION/LIBRARY SPECIFIC ------------ # #
 
     # # # ------------ CODE ------------ # # #
-    iirf100_arr = alpha*(np.sum(a*tau*(1.0 - np.exp(-100.0/(tau*alpha)))))
+    iirf100_arr = alpha*(np.sum((a*tau*(1.0 - np.exp(-100.0/(tau*alpha[:,np.newaxis])))),axis=1))
     return iirf100_arr   -  targ_iirf100
 
-# Define the FAIR simple climate model function
+# Define the FAIR simple climate model function. input_params is the default set of parameters, later we will define each parameter by calling different values (or columns for the case of multiple values for each parameter).
 def fair_scm(tstep=1.0,
              emissions=False,
              other_rf=0.0,
-             co2_concs=False,
-             q=np.array([0.33,0.41]),
-             tcrecs=np.array([1.6,2.75]),
-             d=np.array([239.0,4.1]),
-             a=np.array([0.2173,0.2240,0.2824,0.2763]),
-             tau=np.array([1000000,394.4,36.54,4.304]),
-             r0=32.40,
-             rC=0.019,
-             rT=4.165,
-             F_2x=3.74,
-             C_0=278.0,
-             ppm_gtc=2.123,
-             iirf100_max=97.0,
+             co2_conc=False,
+                                        input_params=np.array([0.33,0.41,1.6,2.75,239.0,4.1,0.2173,0.2240,0.2824,0.2763,1000000,394.4,36.54,4.304,32.40,0.019,4.165,3.74,278.0,2.123,97.0]),
              in_state=[[0.0,0.0,0.0,0.0],[0.0,0.0],0.0],
              restart_out=False):
     """
@@ -106,46 +95,62 @@ def fair_scm(tstep=1.0,
       Atmospheric CO_2 concentrations timeseries (ppmv). If emissions are 
       supplied then co2_concs is not used.
 
-    q:^ (np.array)
-      response of each thermal box to radiative forcing (K/(W/m^2)). 
-      Over-written if tcrecs is supplied (be careful as this is default 
-      behaviour).
+    input_params:^ (np.array)
+      The array which contains all the parameter values. The variables each 
+      column corresponds to are listed below:
+    
+      q:^ (np.array)
+      position = 0,1
+        response of each thermal box to radiative forcing (K/(W/m^2)). 
+        Over-written if tcrecs is supplied (be careful as this is default 
+        behaviour).
 
-    tcrecs:^ (np.array)
-      Transient climate response (TCR) and equilibrium climate sensitivity 
-      (ECS) array (K). tcrecs[0] is TCR and tcrecs[1] is ECS.
+      tcrecs:^ (np.array)
+      position = 2,3
+        Transient climate response (TCR) and equilibrium climate sensitivity 
+        (ECS) array (K). tcrecs[0] is TCR and tcrecs[1] is ECS.
 
-    d:^ (np.array)
-      response time of each thermal box (yrs)
+      d:^ (np.array)
+      position = 4,5
+        response time of each thermal box (yrs)
 
-    a:^ (np.array)
-      fraction of emitted carbon which goes into each carbon pool 
-      (dimensionless)
+      a:^ (np.array)
+      position = 6,7,8,9
+        fraction of emitted carbon which goes into each carbon pool 
+        (dimensionless)
 
-    tau:^ (np.array)
-      unscaled response time of each carbon pool (yrs)
+      tau:^ (np.array)
+      position = 10,11,12,13
+        unscaled response time of each carbon pool (yrs)
 
-    r0:^ (float)
-      pre-industrial 100-year integrated impulse response (iIRF100) (yrs)
+      r0:^ (float)
+      position = 14
+        pre-industrial 100-year integrated impulse response (iIRF100) (yrs)
 
-    rC:^ (float)
-      sensitivity of iIRF100 to CO_2 uptake by the land and oceans (yrs/GtC)
+      rC:^ (float)
+      position = 15
+        sensitivity of iIRF100 to CO_2 uptake by the land and oceans (yrs/GtC)
 
-    rT:^ (float)
-      sensitivity of iIRF100 to increases in global mean temperature (yrs/K)
+      rT:^ (float)
+      position = 16
+        sensitivity of iIRF100 to increases in global mean temperature (yrs/K)
 
-    F_2x:^ (float)
-      radiative forcing due to a doubling of atmospheric CO_2 concentrations 
-      (W/m^2)
+      F_2x:^ (float)
+      position = 17
+        radiative forcing due to a doubling of atmospheric CO_2 concentrations 
+        (W/m^2)
 
-    C_0:^ (float)
-      pre-industrial atmospheric CO_2 concentrations (ppmv)
+      C_0:^ (float)
+      position = 18
+        pre-industrial atmospheric CO_2 concentrations (ppmv)
 
-    ppm_gtc:^ (float)
-      ppmv to GtC conversion factor (GtC/ppmv)
+      ppm_gtc:^ (float)
+      position = 19
+        ppmv to GtC conversion factor (GtC/ppmv)
 
-    iirf100_max:^ (float)
-      maximum allowed value of iIRF100 (keeps the model stable) (yrs)
+      iirf100_max:^ (float)
+      position = 20
+        maximum allowed value of iIRF100 (keeps the model stable) (yrs)
 
     in_state:^ (list/np.array)
       initial state of the climate system with elements:
@@ -196,42 +201,53 @@ def fair_scm(tstep=1.0,
     # # ------------ THIRD PARTY ------------ # #
     import numpy as np
     from scipy.optimize import root
+    import time
 
     # # ------------ LOCAL APPLICATION/LIBRARY SPECIFIC ------------ # #
 
     # # # ------------ CODE ------------ # # #
 
-    # # # ------------ CALCULATE Q ARRAY ------------ # # #
-    # If TCR and ECS are supplied, overwrite the q array
-    k = 1.0 - (d/70.0)*(1.0 - np.exp(-70.0/d))
-    if type(tcrecs) in [np.ndarray,list]:
-        q =  (1.0 / F_2x) * (1.0/(k[0]-k[1])) \
-            * np.array([tcrecs[0]-k[1]*tcrecs[1],k[0]*tcrecs[1]-tcrecs[0]])
-
     # # # ------------ SET UP OUTPUT TIMESERIES VARIABLES ------------ # # #
-    # the integ_len variable is used to store the length of our timeseries
+    begin_time = time.time()
+    # the p_dims variable is used to store the number of different values for each parameter
+    if type(input_params) in [np.ndarray,list]:
+      if input_params.ndim==1:
+        p_dims = 1
+        input_params = input_params.reshape((1,input_params.shape[0]))
+      else:
+        p_dims = input_params.shape[0]
+      
+    # the rf_dims variable is used to store the number of different radiative forcing profiles
+    if type(other_rf) in [np.ndarray,list]:
+      if other_rf.ndim==1:
+        rf_dims = 1
+        other_rf = other_rf[...,np.newaxis].transpose()
+      else:
+        rf_dims = other_rf.shape[0]
+        
+    # the integ_len variable is used to store the length of our timeseries aling with rf_dims and p_dims such that empty parameters of the right dimensions, size and shape can be easily made
     # by default FAIR is not concentration driven
     conc_driven=False
     # here we check if FAIR is emissions driven
     if type(emissions) in [np.ndarray,list]:
-        integ_len = len(emissions)
-        if (type(other_rf) in [np.ndarray,list]) and (len(other_rf)!=integ_len):
+        integ_len = tuple([rf_dims] + [p_dims] + [len(emissions)])
+        if (type(other_rf) in [np.ndarray,list]) and (other_rf.shape[-1]!=integ_len[2]):
             raise ValueError("The emissions and other_rf timeseries don't have the same length")
         elif type(other_rf) in [int,float]:
             other_rf = np.full(integ_len,other_rf)
   
     # here we check if FAIR is concentration driven
     elif type(co2_concs) in [np.ndarray,list]:
-        integ_len = len(co2_concs)
+        integ_len = tuple([rf_dims] + [p_dims] + [len(co2_concs)])
         conc_driven = True
-        if (type(other_rf) in [np.ndarray,list]) and (len(other_rf)!=integ_len):
+        if (type(other_rf) in [np.ndarray,list]) and (other_rf.shape[-1]!=integ_len[2]):
             raise ValueError("The concentrations and other_rf timeseries don't have the same length")
         elif type(other_rf) in [int,float]:
             other_rf = np.full(integ_len,other_rf)
 
     # finally we check if only a non-CO2 radiative forcing timeseries has been supplied
     elif type(other_rf) in [np.ndarray,list]:
-        integ_len = len(other_rf)
+        integ_len = tuple([rf_dims] + [p_dims] + [other_rf.shape[-1]])
         if type(emissions) in [int,float]:
             emissions = np.full(integ_len,emissions)
         else:
@@ -239,18 +255,59 @@ def fair_scm(tstep=1.0,
 
     else:
         raise ValueError("Neither emissions, co2_concs or other_rf is defined as a timeseries")
-
+    
     RF = np.zeros(integ_len)
     C_acc = np.zeros(integ_len)
     iirf100 = np.zeros(integ_len)
 
-    carbon_boxes_shape = (integ_len,4)
+    carbon_boxes_shape = tuple([integ_len[-1]] + [rf_dims] + [p_dims] + [4])
     R_i = np.zeros(carbon_boxes_shape)
     C = np.zeros(integ_len)
 
-    thermal_boxes_shape = (integ_len,2)
+    thermal_boxes_shape = tuple([integ_len[-1]] + [rf_dims] + [p_dims] + [2])
     T_j = np.zeros(thermal_boxes_shape)
     T = np.zeros(integ_len)
+    
+    # Define correct size and shape arrays for each parameter
+    q = np.zeros(thermal_boxes_shape)
+    tcrecs = np.zeros(thermal_boxes_shape)
+    d = np.zeros(thermal_boxes_shape)
+    a = np.zeros(carbon_boxes_shape)
+    tau = np.zeros(carbon_boxes_shape)
+    r0 = np.zeros(integ_len)
+    rC = np.zeros(integ_len)
+    rT = np.zeros(integ_len)
+    F_2x = np.zeros(integ_len)
+    C_0 = np.zeros(integ_len)
+    ppm_gtc = np.zeros(integ_len)
+    iirf100_max = np.zeros(integ_len)
+    
+    # Pull out parameter values from input_params
+    q = input_params[...,0:2]
+    tcrecs = input_params[...,2:4]
+    d = input_params[...,4:6]
+    a = input_params[...,6:10]
+    tau = input_params[...,10:14]
+    r0 = input_params[...,14]
+    rC = input_params[...,15]
+    rT = input_params[...,16]
+    F_2x = input_params[...,17]
+    C_0 = input_params[...,18]
+    ppm_gtc = input_params[...,19]
+    iirf100_max = input_params[...,20]
+    
+    time_scale_sf = np.zeros(integ_len)
+    tau_new = np.zeros(tau.shape)
+    C_out = np.zeros(integ_len)
+    iirf_arr = np.zeros(integ_len)
+    
+        # # # ------------ CALCULATE Q ARRAY ------------ # # #
+    # If TCR and ECS are supplied, overwrite the q array
+    k = np.zeros(d.shape)
+    k = 1.0 - (d/70.0)*(1.0 - np.exp(-70.0/d))
+    if type(tcrecs) in [np.ndarray,list]:
+        q =  ((1.0 / F_2x) * (1.0/(k[...,0]-k[...,1])) \
+            * np.array([tcrecs[...,0]-k[...,1]*tcrecs[...,1],k[...,0]*tcrecs[...,1]-tcrecs[...,0]])).transpose()
 
     # # # ------------ FIRST TIMESTEP ------------ # # #
     R_i_pre = in_state[0]
@@ -264,77 +321,124 @@ def fair_scm(tstep=1.0,
     else:
         # Calculate the parametrised iIRF and check if it is over the maximum 
         # allowed value
-        iirf100[0] = r0 + rC*C_acc_pre + rT*np.sum(T_j_pre)
-        if iirf100[0] >= iirf100_max:
-          iirf100[0] = iirf100_max
+        iirf100[...,0] = r0 + rC*C_acc_pre + rT*np.sum(T_j_pre)
+        for i in range(0,rf_dims):
+            for j in range(0,p_dims):
+                if iirf100[i,j,0] >= iirf100_max[j]:
+                  iirf100[i,j,0] = iirf100_max[j]
           
         # Determine a solution for alpha using scipy's root finder
-        time_scale_sf = (root(iirf100_interp_funct,0.16,args=(a,tau,iirf100[0])))['x']
+            time_scale_sf[i,:,0] = (root(iirf100_interp_funct,0.16,args=(a,tau,iirf100[i,:,0])))['x']
 
         # Multiply default timescales by scale factor
-        tau_new = time_scale_sf * tau
+        tau_new = tau * time_scale_sf[...,0,np.newaxis]
 
         # Compute the updated concentrations box anomalies from the decay of the 
         # previous year and the emisisons
-        R_i[0] = R_i_pre*np.exp(-tstep/tau_new) \
-                  + (emissions[0,np.newaxis])*a*tau_new*(1-np.exp(-tstep/tau_new)) / ppm_gtc
+        R_i[0,...,:] = R_i_pre*np.exp(-tstep/tau_new) \
+                  + (emissions[0,np.newaxis])*a*tau_new*(1-np.exp(-tstep/tau_new)) / ppm_gtc[:,np.newaxis]
 
-        C[0] = np.sum(R_i[0]) + C_0
+        C[...,0] = np.sum(R_i[0,...,:],axis=-1) + C_0
 
         # Calculate the additional carbon uptake
-        C_acc[0] =  C_acc_pre + emissions[0] - (C[0]-(np.sum(R_i_pre) + C_0)) * ppm_gtc
+        C_acc[...,0] =  C_acc_pre + emissions[0] - (C[...,0]-(np.sum(R_i_pre) + C_0)) * ppm_gtc
 
     # Calculate the radiative forcing using the previous timestep's CO2 concentration
-
-    RF[0] = (F_2x/np.log(2.)) * np.log(C_pre/C_0) + other_rf[0]
-
+    RF[...,0] = (F_2x/np.log(2.)) * np.log(C_pre/C_0) + other_rf[:,0,np.newaxis]
+    
     # Update the thermal response boxes
-    T_j[0] = RF[0,np.newaxis]*q*(1-np.exp((-tstep)/d)) + T_j_pre*np.exp(-tstep/d)
+    T_j[0,...,:] = RF[...,0,np.newaxis]*q*(1-np.exp((-tstep)/d)) + T_j_pre*np.exp(-tstep/d)
 
     # Sum the thermal response boxes to get the total temperature anomlay
-    T[0] = np.sum(T_j[0])
-
+    T[...,0] = np.sum(T_j[0,...,:],axis=-1)
+    
     # # # ------------ REST OF RUN ------------ # # #
-    for x in range(1,integ_len):
+    start_time = time.time()
+    
+    for x in range(1,integ_len[-1]):
         if conc_driven:
           C[x] = co2_concs[x]
         
         else:
           # Calculate the parametrised iIRF and check if it is over the maximum 
-          # allowed value
-          iirf100[x] = r0 + rC*C_acc[x-1] + rT*T[x-1]
-          if iirf100[x] >= iirf100_max:
-            iirf100[x] = iirf100_max
+          # allowed value 
+          iirf100[...,x] = r0 + rC*C_acc[...,x-1] + rT*T[...,x-1]
+          for i in range(0,rf_dims):
+            for j in range(0,p_dims):
+                if iirf100[i,j,x] >= iirf100_max[j]:
+                  iirf100[i,j,x] = iirf100_max[j]
             
           # Determine a solution for alpha using scipy's root finder
-          time_scale_sf = (root(iirf100_interp_funct,time_scale_sf,args=(a,tau,iirf100[x])))['x']
+            time_scale_sf[i,:,x] = (root(iirf100_interp_funct,time_scale_sf[i,:,x-1],args=(a,tau,iirf100[i,:,x])))['x']
 
           # Multiply default timescales by scale factor
-          tau_new = time_scale_sf * tau
+          tau_new = time_scale_sf[...,x,np.newaxis] * tau
 
           # Compute the updated concentrations box anomalies from the decay of the previous year and the emisisons
-          R_i[x] = R_i[x-1]*np.exp(-tstep/tau_new) \
-                  + (emissions[x,np.newaxis])*a*tau_new*(1-np.exp(-tstep/tau_new)) / ppm_gtc
+          R_i[x,...,:] = R_i[x-1,...,:]*np.exp(-tstep/tau_new) \
+                  + (emissions[x,np.newaxis])*a*tau_new*(1-np.exp(-tstep/tau_new)) / ppm_gtc[:,np.newaxis]
 
           # Sum the boxes to get the total concentration anomaly
-          C[x] = np.sum(R_i[x]) + C_0
+          C[...,x] = np.sum(R_i[x,...,:],axis=-1) + C_0
 
           # Calculate the additional carbon uptake
-          C_acc[x] =  C_acc[x-1] + emissions[x] * tstep - (C[x]-C[x-1]) * ppm_gtc
+          C_acc[...,x] =  C_acc[...,x-1] + emissions[x] * tstep - (C[...,x]-C[...,x-1]) * ppm_gtc
 
         # Calculate the radiative forcing using the previous timestep's CO2 concentration
-        RF[x] = (F_2x/np.log(2.)) * np.log((C[x-1]) /C_0) + other_rf[x]
+        RF[...,x] = (F_2x/np.log(2.)) * np.log((C[...,x-1]) /C_0) + other_rf[:,x,np.newaxis]
 
         # Update the thermal response boxes
-        T_j[x] = T_j[x-1]*np.exp(-tstep/d) + RF[x,np.newaxis]*q*(1-np.exp(-tstep/d))
+        T_j[x,...,:] = T_j[x-1,...,:]*np.exp(-tstep/d) + RF[...,x,np.newaxis]*q*(1-np.exp(-tstep/d))
         
         # Sum the thermal response boxes to get the total temperature anomaly
-        T[x] = np.sum(T_j[x])
+        T[...,x] = np.sum(T_j[x,...,:],axis=-1)
+        
+        if p_dims + rf_dims > 100:
+          p = round(((x / (integ_len[-1] - 1.))*100.),0)
+          tr = ((((time.time() - start_time) / x) * (integ_len[-1] - 1)) - (time.time() - start_time))
+          tr_mins = int(tr/60.)
+          tr_secs = round((tr/60. - int(tr/60.))*60.,0)
+        
+          if tr_mins == 1.0:
+              print '{0}\r'.format('Completed: %.0f%%. Time remaining: %.0f minute and %.0f seconds.' % (p, tr_mins, tr_secs)),
+          else:
+              print '{0}\r'.format('Completed: %.0f%%. Time remaining: %.0f minutes and %.0f seconds.' % (p, tr_mins, tr_secs)),
 
-    if restart_out:
-        return C, T, (R_i[-1],T_j[-1],C_acc[-1])
+
+   
+    # # # ------------ OUTPUT ------------ # # #
+    if p_dims + rf_dims > 100:
+        mins = int((time.time() - begin_time)/60)
+        secs = round(((time.time() - begin_time)/60 - int((time.time() - begin_time)/60))*60,0)
+        if mins == 1.0:
+          print("---------- Run time was %.0f minute and %.0f seconds ----------" % (mins, secs))
+        else:
+          print("---------- Run time was %.0f minutes and %.0f seconds ----------" % (mins, secs))
+        
+    # Ensure that output form matches input
+    if rf_dims == 1:
+        if p_dims == 1:
+            if restart_out:
+                return C[0,0], T[0,0], (R_i[-1],T_j[-1],C_acc[-1])
+            else:
+                return C[0,0], T[0,0]
+        else:
+            if restart_out:
+                return C[0], T[0], (R_i[-1],T_j[-1],C_acc[-1])
+            else:
+                return C[0], T[0]
     else:
-        return C, T
+        if p_dims == 1:
+            if restart_out:
+                return C[:,0,:], T[:,0,:], (R_i[-1],T_j[-1],C_acc[-1])
+            else:
+                return C[:,0,:], T[:,0,:]
+        else:    
+            if restart_out:
+                return C, T, (R_i[-1],T_j[-1],C_acc[-1])
+            else:
+                return C, T
+        
 
 def plot_fair(emms,
               conc,
