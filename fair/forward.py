9ac464af7c0b9fc84ccc257d18568c605e3fb5a4
Chris Smith <chrisroadmap@gmail.com>
2017-11-16 10:45:23 +0000

    CH4 and N2O forcing relationships successfully implemented with constant natural emissions. Other RF needs to be re-introduced.

--- fair/forward.py
+++ fair/forward.py
@@ -1,8 +1,9 @@
 import inspect
 import numpy as np
 from scipy.optimize import root
-from constants import molwt
+from constants import molwt, lifetime
 from constants.general import M_ATMOS
+from forcing.ghg import etminan
 
 def iirf_interp_funct(alp_b,a,tau,targ_iirf):
 	# ref eq. (7) of Millar et al ACP (2017)
@@ -19,26 +20,36 @@ def fair_scm(emissions=False,
              r0=32.40,
              rc=0.019,
              rt=4.165,
-             F_2x=3.74,
-             C_0=278.0,
+             F2x=3.74,
+             C_0=np.array([278., 722., 275.]),
+             natural=np.array([0, 202., 10.]),
              iirf_max=97.0,
              restart_in=False,
              restart_out=False,
              tcr_dbl=70.0):
 
   # Conversion between ppm CO2 and GtC emissions
-  ppm_gtc = M_ATMOS/1e18*molwt.C/molwt.AIR
+  ppm_gtc   = M_ATMOS/1e18*molwt.C/molwt.AIR
+
+  # Conversion between ppb/ppt concentrations and Mt/kt emissions
+  # in the RCP databases ppb = Mt and ppt = kt so factor always 1e18
+  emis2conc = M_ATMOS/1e18*np.asarray(molwt.aslist)/molwt.AIR
+
+  # Funny units for nitrogen emissions - N2O is expressed in N2 equivalent
+  n2o_sf = molwt.N2O/molwt.N2
+  emis2conc[2] = emis2conc[2] / n2o_sf
 
   # Number of individual gases and radiative forcing agents to consider
-  ngas = 31
-  nF   = 13
+  # just test with CO2 and methane for now
+  ngas = 3
+  nF   = 3
 
   # If TCR and ECS are supplied, calculate the q1 and q2 model coefficients 
   # (overwriting any other q array that might have been supplied)
   # ref eq. (4) and (5) of Millar et al ACP (2017)
   k = 1.0 - (d/tcr_dbl)*(1.0 - np.exp(-tcr_dbl/d))  # Allow TCR to vary
   if type(tcrecs) in [np.ndarray,list]:
-    q =  (1.0 / F_2x) * (1.0/(k[0]-k[1])) * np.array([
+    q =  (1.0 / F2x) * (1.0/(k[0]-k[1])) * np.array([
       tcrecs[0]-tcrecs[1]*k[1],tcrecs[1]*k[0]-tcrecs[0]])
 
   # Convert any list to a numpy array for (a) speed and (b) consistency.
@@ -63,13 +74,13 @@ def fair_scm(emissions=False,
     raise ValueError(
       "Neither emissions or other_rf is defined as a timeseries")
 
-  RF = np.zeros(integ_len)
+  RF = np.zeros((integ_len, nF))
   C_acc = np.zeros(integ_len)
   iirf = np.zeros(integ_len)
   R_i = np.zeros(carbon_boxes_shape)
   T_j = np.zeros(thermal_boxes_shape)
 
-  C = np.zeros(integ_len)
+  C = np.zeros((integ_len, ngas))
   T = np.zeros(integ_len)
 
   if restart_in:
@@ -80,20 +91,19 @@ def fair_scm(emissions=False,
   else:
     # Initialise the carbon pools to be correct for first timestep in
     # numerical method
-    R_i[0,:] = a * emissions[0,np.newaxis] / ppm_gtc
+    R_i[0,:] = a * emissions[0,0] / ppm_gtc   # to add in non-fossil CO2
 
-  C[0] = np.sum(R_i[0,:],axis=-1)
+  # CO2 is a delta from pre-industrial. Other gases are absolute concentration
+  C[0,0] = np.sum(R_i[0,:],axis=-1)
+  C[0,1:] = C_0[1:] - C_0[1:]*(1.0 - np.exp(-1.0/np.array(lifetime.aslist[1:3]))) + (natural[1:] + 0.5 * (emissions[0,1:])) / emis2conc[1:3]
 
-  if type(other_rf) in [float, int]:
-    RF[0] = (F_2x/np.log(2.)) * np.log((C[0] + C_0) /C_0) + other_rf
-  else:
-    RF[0] = (F_2x/np.log(2.)) * np.log((C[0] + C_0) /C_0) + other_rf[0]
+  RF[0,0:3] = etminan(C[0,0:3], C_0[0:3], F2x=F2x)
 
   if restart_in == False:
     # Update the thermal response boxes
-    T_j[0,:] = (q/d)*(RF[0,np.newaxis])
+    T_j[0,:] = (q/d)*(np.sum(RF[0,:]))
 
-  # Sum the thermal response boxes to get the total temperature anomlay
+  # Sum the thermal response boxes to get the total temperature anomaly
   T[0]=np.sum(T_j[0,:],axis=-1)
 
   for x in range(1,integ_len):
@@ -113,27 +123,37 @@ def fair_scm(emissions=False,
     # Multiply default timescales by scale factor
     tau_new = tau * time_scale_sf
 
+    # CARBON DIOXIDE
     # Compute the updated concentrations box anomalies from the decay of the
     # previous year and the additional emissions
-    R_i[x,:] = R_i[x-1,:]*np.exp(-1.0/tau_new) + a*(emissions[x,np.newaxis]) / ppm_gtc
+    R_i[x,:] = R_i[x-1,:]*np.exp(-1.0/tau_new) + a*(emissions[x,0]) / ppm_gtc
     # Sum the boxes to get the total concentration anomaly
-    C[x] = np.sum(R_i[...,x,:],axis=-1)
+    C[x,0] = np.sum(R_i[...,x,:],axis=-1)
     # Calculate the additional carbon uptake
-    C_acc[x] =  C_acc[x-1] + 0.5*(emissions[x] + emissions[x-1]) - (C[x] - C[x-1])*ppm_gtc
+    C_acc[x] =  C_acc[x-1] + 0.5*(emissions[x,0] + emissions[x-1,0]) - (
+      C[x,0] - C[x-1,0])*ppm_gtc
+
+    # METHANE
+    C[x,1] = C[x-1,1] - C[x-1,1]*(1.0 - np.exp(-1.0/lifetime.CH4)) + (
+      natural[1] + 0.5 * (emissions[x,1] + emissions[x-1,1])) / emis2conc[1]
+
+    # NITROUS OXIDE
+    C[x,2] = C[x-1,2] - C[x-1,2]*(1.0 - np.exp(-1.0/lifetime.N2O)) + (
+      natural[2] + 0.5 * (emissions[x,2] + emissions[x-1,2])) / emis2conc[2]
 
     # Calculate the total radiative forcing
-    if type(other_rf) == float:
-      RF[x] = (F_2x/np.log(2.)) * np.log((C[x] + C_0) /C_0) + other_rf
-    else:
-      RF[x] = (F_2x/np.log(2.)) * np.log((C[x] + C_0) /C_0) + other_rf[x]
+    RF[x,0:3] = etminan(C[x,0:3], C_0[0:3], F2x=F2x)
 
     # Update the thermal response boxes
-    T_j[x,:] = T_j[x-1,:]*np.exp(-1.0/d) + q*(1-np.exp((-1.0)/d))*RF[x,np.newaxis]
+    T_j[x,:] = T_j[x-1,:]*np.exp(-1.0/d) + q*(1-np.exp((-1.0)/d))*np.sum(RF[x,:])
     # Sum the thermal response boxes to get the total temperature anomaly
     T[x]=np.sum(T_j[x,:],axis=-1)
 
+  # add delta CO2 concentrations to initial value
+  C[:,0] = C[:,0] + C_0[0]
+
   if restart_out:
     restart_out_val=(R_i[-1],T_j[-1],C_acc[-1])
-    return C + C_0, T, restart_out_val
+    return C, RF, T, restart_out_val
   else:
-    return C + C_0, T
+    return C, RF, T
