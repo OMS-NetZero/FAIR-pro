f5a55f0ef1a375a43809bf9ab2c57a252158868d
Chris Smith <chrisroadmap@gmail.com>
2017-11-17 09:42:50 +0000

    Added in minor WMGHG forcing. Made emissions a compulsory argument to fair_scm in 40-column format

--- fair/forward.py
+++ fair/forward.py
@@ -1,7 +1,7 @@
 import inspect
 import numpy as np
 from scipy.optimize import root
-from constants import molwt, lifetime
+from constants import molwt, lifetime, radeff
 from constants.general import M_ATMOS
 from forcing.ghg import etminan
 
@@ -10,7 +10,7 @@ def iirf_interp_funct(alp_b,a,tau,targ_iirf):
     iirf_arr = alp_b*(np.sum(a*tau*(1.0 - np.exp(-100.0/(tau*alp_b)))))
     return iirf_arr   -  targ_iirf
 
-def fair_scm(emissions=False,
+def fair_scm(emissions,
              other_rf=0.0,
              q=np.array([0.33,0.41]),
              tcrecs=np.array([1.6,2.75]),
@@ -21,8 +21,8 @@ def fair_scm(emissions=False,
              rc=0.019,
              rt=4.165,
              F2x=3.74,
-             C_0=np.array([278., 722., 275.]),
-             natural=np.array([0, 202., 10.]),
+             C_0=np.array([278., 722., 275.] + [0.]*28),
+             natural=np.array([0, 202., 10.] + [0.]*28),
              iirf_max=97.0,
              restart_in=False,
              restart_out=False,
@@ -40,9 +40,9 @@ def fair_scm(emissions=False,
   emis2conc[2] = emis2conc[2] / n2o_sf
 
   # Number of individual gases and radiative forcing agents to consider
-  # just test with CO2 and methane for now
-  ngas = 3
-  nF   = 3
+  # just test with WMGHGs for now
+  ngas = 31
+  nF   = 4
 
   # If TCR and ECS are supplied, calculate the q1 and q2 model coefficients 
   # (overwriting any other q array that might have been supplied)
@@ -61,18 +61,19 @@ def fair_scm(emissions=False,
       exec(i + '= np.array(' + i + ')')
 
   # Set up the output timeseries variables
-  if type(emissions) is np.ndarray:
-    carbon_boxes_shape = (emissions.shape[0], a.shape[0])
-    thermal_boxes_shape = (emissions.shape[0], d.shape[0])
-    integ_len = emissions.shape[0]
-  elif type(other_rf) is np.ndarray:
-    carbon_boxes_shape = (other_rf.shape[0], a.shape[0])
-    thermal_boxes_shape = (other_rf.shape[0], d.shape[0])
-    integ_len = len(other_rf)
-    emissions = np.zeros(integ_len)
-  else:
-    raise ValueError(
-      "Neither emissions or other_rf is defined as a timeseries")
+  if type(emissions) is not np.ndarray or emissions.shape[1] != 40:
+    raise ValueError("emissions timeseries should be a nt x 40 numpy array")
+  carbon_boxes_shape = (emissions.shape[0], a.shape[0])
+  thermal_boxes_shape = (emissions.shape[0], d.shape[0])
+  integ_len = emissions.shape[0]
+#  elif type(other_rf) is np.ndarray:
+#    carbon_boxes_shape = (other_rf.shape[0], a.shape[0])
+#    thermal_boxes_shape = (other_rf.shape[0], d.shape[0])
+#    integ_len = len(other_rf)
+#    emissions = np.zeros(integ_len)
+#  else:
+#    raise ValueError(
+#      "Neither emissions or other_rf is defined as a timeseries")
 
   RF = np.zeros((integ_len, nF))
   C_acc = np.zeros(integ_len)
@@ -87,17 +88,23 @@ def fair_scm(emissions=False,
     R_i[0]=restart_in[0]
     T_j[0]=restart_in[1]
     C_acc[0] = restart_in[2]
-
   else:
     # Initialise the carbon pools to be correct for first timestep in
     # numerical method
-    R_i[0,:] = a * emissions[0,0] / ppm_gtc   # to add in non-fossil CO2
+    R_i[0,:] = a * (np.sum(emissions[0,1:3])) / ppm_gtc
 
   # CO2 is a delta from pre-industrial. Other gases are absolute concentration
   C[0,0] = np.sum(R_i[0,:],axis=-1)
-  C[0,1:] = C_0[1:] - C_0[1:]*(1.0 - np.exp(-1.0/np.array(lifetime.aslist[1:3]))) + (natural[1:] + 0.5 * (emissions[0,1:])) / emis2conc[1:3]
+  C[0,1:3] = C_0[1:3] - C_0[1:3]*(1.0 - np.exp(-1.0/np.array(lifetime.aslist[1:3]))) + (natural[1:3] + 0.5 * (emissions[0,2:4])) / emis2conc[1:3]
+  C[0,3:] = C_0[3:] - C_0[3:]*(1.0 - np.exp(-1.0/np.array(lifetime.aslist[3:]))) + (natural[3:] + 0.5 * (emissions[0,12:])) / emis2conc[3:]
 
+  # CO2, CH4 and methane are co-dependent and from Etminan relationship
   RF[0,0:3] = etminan(C[0,0:3], C_0[0:3], F2x=F2x)
+
+  # Minor (F- and H-gases) are linear in concentration
+  # the factor of 0.001 here is because radiative efficiencies are given
+  # in W/m2/ppb and concentrations of minor gases are in ppt.
+  RF[0,3] = np.sum((C[0,3:] - C_0[3:]) * radeff.aslist[3:] * 0.001)
 
   if restart_in == False:
     # Update the thermal response boxes
@@ -126,23 +133,29 @@ def fair_scm(emissions=False,
     # CARBON DIOXIDE
     # Compute the updated concentrations box anomalies from the decay of the
     # previous year and the additional emissions
-    R_i[x,:] = R_i[x-1,:]*np.exp(-1.0/tau_new) + a*(emissions[x,0]) / ppm_gtc
+    R_i[x,:] = R_i[x-1,:]*np.exp(-1.0/tau_new) + a*(np.sum(emissions[x,1:3])) / ppm_gtc
     # Sum the boxes to get the total concentration anomaly
     C[x,0] = np.sum(R_i[...,x,:],axis=-1)
     # Calculate the additional carbon uptake
-    C_acc[x] =  C_acc[x-1] + 0.5*(emissions[x,0] + emissions[x-1,0]) - (
+    C_acc[x] =  C_acc[x-1] + 0.5*(np.sum(emissions[x-1:x+1,1:3])) - (
       C[x,0] - C[x-1,0])*ppm_gtc
 
     # METHANE
     C[x,1] = C[x-1,1] - C[x-1,1]*(1.0 - np.exp(-1.0/lifetime.CH4)) + (
-      natural[1] + 0.5 * (emissions[x,1] + emissions[x-1,1])) / emis2conc[1]
+      natural[1] + 0.5 * (emissions[x,3] + emissions[x-1,3])) / emis2conc[1]
 
     # NITROUS OXIDE
     C[x,2] = C[x-1,2] - C[x-1,2]*(1.0 - np.exp(-1.0/lifetime.N2O)) + (
-      natural[2] + 0.5 * (emissions[x,2] + emissions[x-1,2])) / emis2conc[2]
+      natural[2] + 0.5 * (emissions[x,4] + emissions[x-1,4])) / emis2conc[2]
+
+    # OTHER WMGHGs
+    C[x,3:] = C[x-1,3:] - C[x-1,3:]*(1.0 - np.exp(-1.0/np.array(
+      lifetime.aslist[3:]))) + (natural[3:] + 0.5 * (
+      emissions[x,12:] + emissions[x-1,12:])) / emis2conc[3:]
 
     # Calculate the total radiative forcing
     RF[x,0:3] = etminan(C[x,0:3], C_0[0:3], F2x=F2x)
+    RF[x,3] = np.sum((C[x,3:] - C_0[3:]) * radeff.aslist[3:] * 0.001)
 
     # Update the thermal response boxes
     T_j[x,:] = T_j[x-1,:]*np.exp(-1.0/d) + q*(1-np.exp((-1.0)/d))*np.sum(RF[x,:])
